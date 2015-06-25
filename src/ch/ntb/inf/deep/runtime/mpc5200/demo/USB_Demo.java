6eec4ac4d95268b0bb51c541741110b2ce4f5219
Andreas Kalberer <andreas.kalberer@ntb.ch>
2015-06-25 15:50:34 +0200

    GPS port config, init for usb started

--- src/ch/ntb/inf/deep/runtime/mpc5200/demo/USB_Demo.java
+++ src/ch/ntb/inf/deep/runtime/mpc5200/demo/USB_Demo.java
@@ -1,10 +1,24 @@
 package ch.ntb.inf.deep.runtime.mpc5200.demo;
 
+import java.io.PrintStream;
+
+import ch.ntb.inf.deep.runtime.mpc5200.driver.UART3;
 import ch.ntb.inf.deep.runtime.mpc5200.driver.usb.USB;
 
-public class USB_Demo {
-
+public class USB_Demo{
+	
 	static{
+		
+		// Initialize UART (9600 8N1)
+		UART3.start(9600, UART3.NO_PARITY, (short)8);
+
+		// Use the UART3 for stdout and stderr
+		System.out = new PrintStream(UART3.out);
+		System.err = System.out;
+
+		// Print a string to the stdout
+		System.out.println("USB Demo:");
+		
 		USB usb = new USB();
 		usb.init();
 	}
--- src/ch/ntb/inf/deep/runtime/mpc5200/driver/usb/USB.java
+++ src/ch/ntb/inf/deep/runtime/mpc5200/driver/usb/USB.java
@@ -7,7 +7,24 @@ public class USB implements IphyCoreMpc5200io{
 
 	public void init(){
 		//init CDM Fractional Divider Config Register (internal USB Clock 48 MHz)
-		US.PUT4(MemBaseAddr+CDMFDCR, 0x05015555);
+		US.PUT4(CDMFDCR, 0x04015555);
+		//init GPS port config register for USB support
+		int tmp = US.GET4(GPSPCR);
+		tmp |= 0x00001000;
+		tmp &= ~0x00002000;
+		US.PUT4(GPSPCR, tmp);
 		
+		US.PUT4(USBHCRHSR, 0x00010000);
+		
+		long cnt = 0;
+		while( (US.GET4(USBHCRHP1SR)&0x001F0010) == 0 ){
+			if(cnt > 10000000){
+				System.out.println(".");
+				cnt = 0;
+			}
+			cnt++;
+		}
+		
+		System.out.println("Port status changed.");
 	}
 }
